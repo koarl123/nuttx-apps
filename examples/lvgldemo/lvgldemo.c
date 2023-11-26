41f9d9697466652f0394f9443af4cd44299c2a06
karl <sb0nschbob@web.de>
2023-11-26 14:15:37 +0100

    fix bug

--- examples/lvgldemo/lvgldemo.c
+++ examples/lvgldemo/lvgldemo.c
@@ -36,7 +36,6 @@
 #include <lvgl/lvgl.h>
 #include <port/lv_port.h>
 #include <lvgl/demos/lv_demos.h>
-#include "lvgl_centeredbutton.h"
 #ifdef CONFIG_LIBUV
 #  include <uv.h>
 #  include <port/lv_port_libuv.h>
@@ -67,6 +66,7 @@
  ****************************************************************************/
 
 typedef CODE void (*demo_create_func_t)(void);
+static void myview_centered_button(void);
 
 struct func_key_pair_s
 {
@@ -106,7 +106,35 @@ static const struct func_key_pair_s func_key_pair[] =
 /****************************************************************************
  * Private Functions
  ****************************************************************************/
+static void btn_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * btn = lv_event_get_target(e);
+    if(code == LV_EVENT_CLICKED) {
+        static uint8_t cnt = 0;
+        cnt++;
 
+        /*Get the first child of the button which is the label and change its text*/
+        lv_obj_t * label = lv_obj_get_child(btn, 0);
+        lv_label_set_text_fmt(label, "Button: %d", cnt);
+    }
+}
+
+/**
+ * Create a button with a label and react on click event.
+ */
+static void myview_centered_button(void)
+{
+    lv_obj_t * btn = lv_btn_create(lv_scr_act());     /*Add a button the current screen*/
+    lv_obj_set_pos(btn, 60, 160-25);                            /*Set its position*/
+    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
+    lv_obj_add_flag(btn, LV_OBJ_FLAG_CLICKABLE);
+    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
+
+    lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
+    lv_label_set_text(label, "Button");                     /*Set the labels text*/
+    lv_obj_center(label);
+}
 /****************************************************************************
  * Name: show_usage
  ****************************************************************************/
